0a860e0d4f578f524aa4a1a5bec9fe2c75b68e3a
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-03-24 15:10:39 -0400

    Update apuntesCurrying.hs

--- apuntesCurrying.hs
+++ apuntesCurrying.hs
@@ -8,3 +8,12 @@ f2 :: p -> p
 f2 x = x
 --Al evaluar las siguientes expresiones tendra la respuesta
 --f1 (*2) 4
+{-
+f2::a -> a
+f1:: tx -> ty ->tr
+f1:: tx -> ty ->tr
+f1:(ty -> tr)-> (ty ->tr)
+-}
+f9 :: (t1 -> t2 -> t3) -> t1 -> t2 -> t3
+f9 x y z = (x y) z
+-- f9 x y z = x y z
