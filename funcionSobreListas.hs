6d9485f716dac4286424eae866a2bcbefde6d3f8
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-03-30 09:32:32 -0400

    Update funcionSobreListas.hs

--- funcionSobreListas.hs
+++ funcionSobreListas.hs
@@ -39,4 +39,33 @@ head :: [a] -> a
 []
 *Main> head [(+),(*)] 2 3
 5
+
+TAIL
+tail :: [a] -> [a]
+
+*Main> tail [1,2,7,8]
+[2,7,8]
+*Main> tail [[1,2,3]]
+[]
+*Main> tail [[[]]]
+[]
+*Main> tail [[]]
+[]
+*Main> tail [1]
+[]
+*Main> tail []
+*** Exception: Prelude.tail: empty list
+
+*Main> tail [(+),(*)] [2,5]
+
+<interactive>:40:1: error:
+    • Couldn't match expected type ‘[Integer] -> t’
+                  with actual type ‘[Integer -> Integer -> Integer]’
+    • The function ‘tail’ is applied to two arguments,
+      but its type ‘[Integer -> Integer -> Integer]
+                    -> [Integer -> Integer -> Integer]’
+      has only one
+      In the expression: tail [(+), (*)] [2, 5]
+      In an equation for ‘it’: it = tail [(+), (*)] [2, 5]
+    • Relevant bindings include it :: t (bound at <interactive>:40:1)
 -}
