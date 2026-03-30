3c5a781d38fd3c310d7e0c4e4126b7ce543b17c2
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-03-30 15:27:03 -0400

    Create calclambda.hs

---
+++ EjerciciosPF2026/calclambda.hs
@@ -0,0 +1,9 @@
+{-- calculo lambda
+f que reciba 3 numeros dev  "menor" si la
+sumatoria es menor que 10 "mayor" si sum mayor a 20
+"vacio" en otro caso--}
+
+mje x y z =
+    let
+        suma = x+y+z 
+        
