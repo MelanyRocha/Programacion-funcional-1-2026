ac559d88d336463051efc02e029cf60ef7f93296
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-03-30 15:29:26 -0400

    Create apuntesCurrying.hs

---
+++ EjerciciosPF2024-2025/apuntesCurrying.hs
@@ -0,0 +1,26 @@
+{- Simplificacion de funciones
+
+-}
+
+f1 :: (t1 -> t2) -> t1 -> t2
+f1 x y = x y
+f2 :: p -> p
+f2 x = x
+--Al evaluar las siguientes expresiones tendra la respuesta
+--f1 (*2) 4
+{-
+f2::a -> a
+f1:: tx -> ty ->tr
+f1:: tx -> ty ->tr
+f1:(ty -> tr)-> (ty ->tr)
+-}
+f9 :: (t1 -> t2 -> t3) -> t1 -> t2 -> t3
+f9 x y z = (x y) z
+-- f9 x y z = x y z
+f3 :: Integer -> Integer
+f3= f2 (*) 4
+f4 :: Integer -> Integer
+f4 = (f1 (*)) 4
+--f5 = f1 4 -- da error f1 espera una funcion por eso no funciona
+f6 = f2 4 -- f2 es mas general que f1 por eso acepta todo
+--- para el martes 31/03/2026 repaso de currying
