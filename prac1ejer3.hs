86b5d46a78b4b8803ea63e1f0b7d2c3bb70e6ed4
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-03-30 15:25:37 -0400

    Create prac1ejer3.hs

---
+++ prac1ejer3.hs
@@ -0,0 +1,43 @@
+--EJERCICIO 3 (Resolver usando Distinción de casos)
+--1. Definir una función que reciba 4 número y devuelva el mayor.
+-- Por combinación.
+
+-- Por distinción de casos.
+
+--2. Definir una función que reciba una nota 
+--y devuelva el mensaje “Aprobado” o “Reprobado”.
+
+--3. Definir una función que reciba una nota y 
+--devuelva el mensaje “Excelente“ si la nota
+--esta entre 90-100, “Bien” si esta entre 70-89, 
+-- “Regular” si esta entre 51-69 y mal si esta entre 0-50.
+
+
+--4. Definir una función que reciba como argumentos las 
+--notas de primer parcial, segundo parcial, final y segunda instancia 
+--y retorne el mensaje aprobado o reprobado, según el caso.
+
+
+--5. Definir una función que reciba 16 números y retorne el mayor
+
+--6. Definir una función que reciba un quebrado y devuelva verdad
+ --si este es mayor que 1 y falso en otro caso
+
+
+--7. Definir una función que reciba 2 fechas y devuelva la fecha mayor
+
+
+--8. Definir una función que reciba 2 fechas y devuelva los años transcurridos
+
+
+--9. Definir una función que reciba 2 fechas y devuelva los meses transcurridos
+
+
+--10. Definir una función que reciba 2 fechas y devuelva los días transcurridos
+
+
+--11. Definir una función que reciba 2 fechas y devuelva los días, meses y años
+--transcurridos
+
+--12. Definir una función que reciba un instante (fecha, hora)
+ --e incremente el instante en 1 segundo.
