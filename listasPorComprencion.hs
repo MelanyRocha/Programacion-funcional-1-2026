662ce95a8b338f76b4518ab74341e7c344270958
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-04-20 08:46:00 -0400

    Create listasPorComprencion.hs

---
+++ listasPorComprencion.hs
@@ -0,0 +1,2 @@
+-- {x/xE lN, x<5} = [1,2,3,4]
+[x | x <- [1..], x < 5] -- esto es una lista por comprensión, se lee "la lista de x tal que x pertenece a la lista de los numeros naturales y x es menor que 5"
