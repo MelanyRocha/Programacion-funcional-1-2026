fdc11132de367e36f5ac0d9535abe12d818b0aeb
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-04-27 11:58:30 -0400

    Create resolucionEjerPP.hs

---
+++ resolucionEjerPP.hs
@@ -0,0 +1,13 @@
+--Sea:
+-- f :: (t1 -> t2) -> t1 -> t2
+f x y = x y
+
+h n1 n2 n3 = n1+n2+n3
+h:: Int -> Int -> Int -> Int
+-- Escribir el tipo de :
+--f h:: 
+
+
+
+
+-- h puede ser argumento de f?  
