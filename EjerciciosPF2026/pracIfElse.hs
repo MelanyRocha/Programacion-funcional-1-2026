3f74be4ffcb72b28d834fb35f0e3b36c83ba4975
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-08-19 23:41:22 -0400

    Update pracIfElse.hs

--- EjerciciosPF2026/pracIfElse.hs
+++ EjerciciosPF2026/pracIfElse.hs
@@ -1,5 +1,6 @@
 --EJERCICIO 2 (Resolver usando Expresiones if)
 --1. Definir una función que devuelva el mayor de 2 números
+mayor2num x y = if x>y then x else y
 --2. Definir una función que reciba 3 números y devuelva el mayor
 --3. Definir una función que reciba 4 números y devuelva el mayor
 --10. Definir una función que reciba dos exámenes parciales, un final y una segunda
