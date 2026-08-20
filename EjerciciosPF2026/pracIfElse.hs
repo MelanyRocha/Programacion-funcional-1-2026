4a43827a787e5f17a44c7f95f3ac80b40f9e2867
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-08-19 23:38:58 -0400

    Update pracIfElse.hs

--- EjerciciosPF2026/pracIfElse.hs
+++ EjerciciosPF2026/pracIfElse.hs
@@ -1,8 +1,8 @@
-EJERCICIO 2 (Resolver usando Expresiones if)
-1. Definir una función que devuelva el mayor de 2 números
-2. Definir una función que reciba 3 números y devuelva el mayor
-3. Definir una función que reciba 4 números y devuelva el mayor
-10. Definir una función que reciba dos exámenes parciales, un final y una segunda
-instancia y devuelva el mensaje “Aprobado”, “Reprobado” o “Abandono” según el
-caso.
-11. Definir una función que reciba 2 fechas y devuelva la fecha mayor
+--EJERCICIO 2 (Resolver usando Expresiones if)
+--1. Definir una función que devuelva el mayor de 2 números
+--2. Definir una función que reciba 3 números y devuelva el mayor
+--3. Definir una función que reciba 4 números y devuelva el mayor
+--10. Definir una función que reciba dos exámenes parciales, un final y una segunda
+--instancia y devuelva el mensaje “Aprobado”, “Reprobado” o “Abandono” según el
+--caso.
+--11. Definir una función que reciba 2 fechas y devuelva la fecha mayor
